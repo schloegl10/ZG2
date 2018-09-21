952e5237f8fcb24993b7a528aa43d677263d9d61
schloegl10 <43451272+schloegl10@users.noreply.github.com>
2018-09-20 22:19:51 -0300

    DAO_PRomocao quase completo

--- src/caixa/DAO_Promocoes.java
+++ src/caixa/DAO_Promocoes.java
@@ -6,19 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DAO_Promocoes {
-	private static final String INSERT_SQL = "INSERT INTO";
+	private static final String INSERT_SQL = "INSERT INTO PROMOCOES(ID, DESCRICAO, OBSERVACAO, QUANTIDADE_ATIVACAO, PRECO_FINAL, QUANTIDADE_PAGA) VALUES(?, ?, ?, ?, ?, ?)";
 	private static final String SELECT_SQL = "SELECT INTO";
 	private static final String UPDATE_SQL = "UPDATE INTO";
 	private static final String DELETE_SQL = "DELETE INTO";
 	public void gerarPromocao(Promocao promocao) throws SQLException {
 		try(Connection conexao = FabricaConexao.getConexao();
 				PreparedStatement consulta = conexao.prepareStatement(SELECT_SQL)){
-			consulta.setInt(1, promocao);
-			consulta.setString(2,);
-			consulta.setString(3,);
-			consulta.setString(4,);
-			consulta.setString(5,):
-			consulta.setString(6,);
+			consulta.setInt(1, promocao.getID());
+			consulta.setString(2,promocao.getDescricao());
+			consulta.setString(3, promocao.getObservacao());
+			consulta.setInt(4, promocao.getQuantidadeAtivacao());
+			consulta.setBigDecimal(5,):
+			consulta.setInt(6, promocao.get);
 			consulta.executeUpdate(
 					);
 		}
@@ -40,6 +40,7 @@ public class DAO_Promocoes {
 				else {
 					promocao = new PromocaoPagueLeve(resultado.getInt("ID"), resultado.getString("DESCRICAO"), resultado.getString("OBSERVACAO"), resultado.getInt("QUANTIDADE_ATIVACAO"), resultado.getInt("QUANTIDADE_PAGA"));
 				}
+				return promocao;
 			}
 		}
 		catch(SQLException e){
